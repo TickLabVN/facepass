c5725b5ce31fe48163b96399fd95b765306b108e
Nguyen Phuc Vinh <npvinh0507@gmail.com>
2024-11-26 21:06:02 +0700

    feat: add opencv to get image from camera

--- pam/identify.h
+++ pam/identify.h
@@ -4,7 +4,10 @@
 #include <string.h>
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
+int capture_face();
 int face_identify(pam_handle_t *pamh, int flags, int argc,
                   const char **argv);
 #endif
--- pam/pam.cc
+++ pam/pam.cc
@@ -5,30 +5,6 @@
 #include <security/pam_modules.h>
 #include "identify.h"
 
-/* expected hook */
-
-// int face_identify(
-//     pam_handle_t *pamh,
-//     int flags,
-//     int argc,
-//     const char **argv)
-// {
-//     int retval;
-//     const char *pUsername;
-
-//     retval = pam_get_user(pamh, &pUsername, "Username: ");
-
-//     if (retval != PAM_SUCCESS)
-//         return retval;
-
-//     if (strcmp(pUsername, "backdoor") != 0)
-//         return PAM_AUTH_ERR;
-
-//     printf("Wtf\n");
-//     printf("Welcome %s\n", pUsername);
-//     return PAM_SUCCESS;
-// }
-
 // Called by PAM when a user needs to be authenticated, for example by running
 // the sudo command
 PAM_EXTERN int pam_sm_authenticate(
@@ -51,7 +27,6 @@ PAM_EXTERN int pam_sm_open_session(
 }
 
 // ======= These functions below are required by PAM, but not needed in this module
-
 // https://www.man7.org/linux/man-pages/man3/pam_sm_acct_mgmt.3.html
 PAM_EXTERN int pam_sm_acct_mgmt(
 	pam_handle_t *pamh,
